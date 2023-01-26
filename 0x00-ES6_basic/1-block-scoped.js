cbf3148f5b8638b8210703c438c65fa0b653b7ae
Momanyi Poet <101427637+MomanyiPoet@users.noreply.github.com>
2023-01-26 16:01:02 +0300

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,3 +1,7 @@
+/**
+ * If trueOrFalse is true, then return [true, false], otherwise return [false, true].
+ */
+
 export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
