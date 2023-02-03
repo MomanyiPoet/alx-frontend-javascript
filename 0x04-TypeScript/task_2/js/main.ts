73fc6a1ee19cfd89376816a2de18bf05abb887d6
Momanyi Poet <101427637+MomanyiPoet@users.noreply.github.com>
2023-02-03 16:18:39 +0300

    String literal types

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,71 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome() {
+    return 'Working from home';
+  }
+  getCoffeeBreak() {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks() {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome() {
+    return 'Cannot work from home';
+  }
+  getCoffeeBreak() {
+    return 'Cannot have a break';
+  }
+  workTeacherTasks() {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  }
+  if (typeof salary === 'string') {
+    const salaryNum = parseInt(salary.slice(1));
+    if (salaryNum < 500) {
+      return new Teacher();
+    }
+  }
+  return new Director();
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects) {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+}
